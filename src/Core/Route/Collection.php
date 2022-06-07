9034ed258e3121d14bc4f777312e6d921808782f
Helmut Januschka <h.januschka@krone.at>
2022-06-07 12:15:43 +0000

    Update Collection.php

--- src/Core/Route/Collection.php
+++ src/Core/Route/Collection.php
@@ -59,6 +59,7 @@ final class Collection implements Common\Route\Collection {
 	 *
 	 * @return \ArrayIterator Iterator object.
 	 */
+	 #[\ReturnTypeWillChange]
 	public function getIterator() {
 
 		return new \ArrayIterator( $this->routes );
